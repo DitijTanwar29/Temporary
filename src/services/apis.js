3ea9948d951ad8d1471e547d7520c25fa1619498
DitijTanwar29 <ditijtanwar2001@gmail.com>
2024-02-10 22:56:31 +0530

    Last try

--- src/services/apis.js
+++ src/services/apis.js
@@ -4,7 +4,7 @@ const BASE_URL = "https://surucucv-ejm4.onrender.com/api/v1";
 export const endpoints = {
     // SENDOTP_API: BASE_URL + "/auth/sendotp",
     SIGNUP_API: BASE_URL + "/auth/signup",
-    LOGIN_API: BASE_URL + "auth/login",
+    LOGIN_API: "https://surucucv-ejm4.onrender.com/api/v1/auth/login",
     // RESETPASSTOKEN_API: BASE_URL + "/auth/reset-password-token",
     // RESETPASSWORD_API: BASE_URL + "/auth/reset-password"
 }
